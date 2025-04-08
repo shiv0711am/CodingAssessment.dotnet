8c99db784a02544a9f75745c11d3db65649d267c
shivamaggarwal0711 <shivamaggarwal0711@gmail.com>
2025-04-09 01:12:58 +0530

    Code review part 1

--- CodeToReview.cs
+++ CodeToReview.cs
@@ -4,11 +4,20 @@ using System.Linq;
 
 namespace Utility.Valocity.ProfileHelper
 {
+    //indendation of the class fields and methods should be corrected for readability.
     public class People
     {
-     private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
+        // Under16 can be declared const not readonly.
+        /* 
+         
+            As this const Under16 will not be used if the single parameter constructor is removed,
+            thus this can also be removed from the class.
+
+        */
+        private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
      public string Name { get; private set; }
      public DateTimeOffset DOB { get; private set; }
+        // As this constructor is not being used in the code currently, therefore it can be removed.
      public People(string name) : this(name, Under16.Date) { }
      public People(string name, DateTime dob) {
          Name = name;
@@ -20,6 +29,7 @@ namespace Utility.Valocity.ProfileHelper
         /// <summary>
         /// MaxItemsToRetrieve
         /// </summary>
+        /// This field can be made readonly 
         private List<People> _people;
 
         public BirthingUnit()
@@ -32,13 +42,15 @@ namespace Utility.Valocity.ProfileHelper
         /// </summary>
         /// <param name="j"></param>
         /// <returns>List<object></returns>
+        /// The method name does not rightly suggests what it is doing,rename it to AddPeople
         public List<People> GetPeople(int i)
         {
             for (int j = 0; j < i; j++)
             {
                 try
                 {
-                    // Creates a dandon Name
+                    // Creates a random Name
+                    // The logic of determing the name can be moved to a separate method GetName() to follow SRP
                     string name = string.Empty;
                     var random = new Random();
                     if (random.Next(0, 1) == 0) {
@@ -48,6 +60,7 @@ namespace Utility.Valocity.ProfileHelper
                         name = "Betty";
                     }
                     // Adds new people to the list
+                    // The calculation of determing a random DOB can be moved to a separate method, GetDOB()
                     _people.Add(new People(name, DateTime.UtcNow.Subtract(new TimeSpan(random.Next(18, 85) * 356, 0, 0, 0))));
                 }
                 catch (Exception e)
@@ -61,6 +74,7 @@ namespace Utility.Valocity.ProfileHelper
 
         private IEnumerable<People> GetBobs(bool olderThan30)
         {
+            //x.Name == "Bob" can be written as x.Name.Equals("Bob", StringComparison.InvariantCultureIgnoreCase)
             return olderThan30 ? _people.Where(x => x.Name == "Bob" && x.DOB >= DateTime.Now.Subtract(new TimeSpan(30 * 356, 0, 0, 0))) : _people.Where(x => x.Name == "Bob");
         }
 
@@ -68,6 +82,8 @@ namespace Utility.Valocity.ProfileHelper
         {
             if (lastName.Contains("test"))
                 return p.Name;
+
+            // the entire condition can be re-written as 'return (p.Name + " " + lastName).Substring(0, 255);'
             if ((p.Name.Length + lastName).Length > 255)
             {
                 (p.Name + " " + lastName).Substring(0, 255);
