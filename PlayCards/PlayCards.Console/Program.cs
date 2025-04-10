743310ad6a53eb1f659215ae4e2977a178057ee6
shivamaggarwal0711 <shivamaggarwal0711@gmail.com>
2025-04-11 02:25:26 +0530

    Added functionality to console which will tell if application works.

--- PlayCards/PlayCards.Console/Program.cs
+++ PlayCards/PlayCards.Console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PlayCards.Core;
+using PlayCards.Core.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var shuffleService = new SuffleService();
+        var deck = new Deck(shuffleService);
+
+        deck.Shuffle();
+        Console.WriteLine("Shuffled deck:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(deck.Draw());
+        }
+    }
+}
